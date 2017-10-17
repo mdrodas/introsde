cd50cdc5d638f0675d5a35182dfc897a708b6c4f
Marcelo Rodas <marcelo.rodasbritez@unitn.it>
2017-10-17 16:08:15 +0200

    updates on ivy libraries of lab06 and main App.

--- lab06/Examples/src/introsde/rest/ehealth/App.java
+++ lab06/Examples/src/introsde/rest/ehealth/App.java
@@ -7,18 +7,19 @@ import java.net.URISyntaxException;
 import org.glassfish.jersey.jdkhttp.JdkHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 
-public class App
-{
-    private static final URI BASE_URI = URI.create("http://localhost:5900/sdelab/");	
-    public static void main(String[] args) throws IllegalArgumentException, IOException, URISyntaxException
-    {
-    	System.out.println("Starting sdelab standalone HTTP server...");
-        JdkHttpServerFactory.createHttpServer(BASE_URI, createApp());
-        System.out.println("Server started on " + BASE_URI + "\n[kill the process to exit]");
-    }
+public class App {
+	private static final URI BASE_URI = URI.create("http://localhost:5900/sdelab/");
 
-    public static ResourceConfig createApp() {
-    	System.out.println("Starting sdelab REST services...");
-        return new MyApplicationConfig();
-    }
+	public static void main(String[] args) throws IllegalArgumentException, IOException, URISyntaxException {
+		System.out.println("Starting sdelab standalone HTTP server...");
+		ResourceConfig rc = createApp();
+		JdkHttpServerFactory.createHttpServer(BASE_URI, rc);
+		System.out.println("Server started on " + BASE_URI + "\n[kill the process to exit]");
+	}
+
+	public static ResourceConfig createApp() {
+		System.out.println("Starting sdelab REST services...");
+		MyApplicationConfig config = new MyApplicationConfig();
+		return config;
+	}
 }
